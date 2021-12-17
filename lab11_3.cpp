6e21a407afa9fc5090520f82622f3bcc8d8c8252
SupasanRuang <supasan_ruang@cmu.ac.th>
2021-12-17 13:50:53 +0700

    lab11_3.cpp completed

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,42 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#define _USE_MATH_DEFINES
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<cmath>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    ifstream score;
+    int count=0;
+    double sum=0,sum_pow_2=0,mean=0,Standard_deviation; 
+    score.open("score.txt") ;
+    string textline;
+    double num;
+	while (getline(score,textline))
+	{
+		num=stof(textline);
+       
+        count++;
+        sum+=num;
+        sum_pow_2+=(num*num);
+       
+	}
+    mean=sum/count;
+    Standard_deviation=sqrt((sum_pow_2/count)-(mean*mean));
+    cout << "Number of data = "<< count<<endl;
+    cout << setprecision(3);
+    cout << "Mean = "<< mean<<endl;
+    cout << "Standard deviation = "<< Standard_deviation<<endl;
+
+    
+
+
+    return 0;
+}
+
+
+
+
+
